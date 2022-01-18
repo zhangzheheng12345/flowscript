ff63cf718bec5882918964c8bb87cf7fac2f5218
zhangzheheng12345@163.com <zhangzheheng12345@163.com>
2022-01-18 21:19:22 +0800

    Fix errors

--- xlexer/xlex.go
+++ xlexer/xlex.go
@@ -1,60 +1,67 @@
 package xlexer
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
 /* Check if a character is a number. */
 func IsSingleNum(str string) bool {
-    return len(str) == 1 && []byte(str)[0] >= 48 && []byte(str)[0] <= 57
+	return len(str) == 1 && []byte(str)[0] >= 48 && []byte(str)[0] <= 57
 }
 
 /* Check if a character is a alpha */
 func IsSingleAlpha(str string) bool {
-    return len(str) == 1 && []byte(str)[0] >= 65 && []byte(str)[0] <= 122
+	return len(str) == 1 && []byte(str)[0] >= 65 && []byte(str)[0] <= 122
 }
 
 func Lex(str []string) []Token {
-    result := make([]Token,0)
-    for index := 0; index < len(str); index++ {
-        if str[index] == " " || str[index] == "\t" {
-            // Do nothing
-        } else if str[index] == "_" || IsSingleAlpha(str[index]) {
-            begin := index
-            for ;index < len(str) &&
-                ( IsSingleAlpha(str[index]) ||
-                IsSingleNum(str[index]) ||
-                str[index] == "_" );index++ {}
-            result = append(result,Token{SYMBOL, strings.Join(str[begin:index], "")})
-            index--
-        } else if IsSingleNum(str[index]) {
-            begin := index
-            for ;index < len(str) && IsSingleNum(str[index]);index++ {}
-            result = append(result,Token{NUM, strings.Join(str[begin:index], "")})
-            index--
-        } else if str[index] == "(" {
-            result = append(result,Token{FPAREN,""})
-        }  else if str[index] == ")" {
-            result = append(result,Token{BPAREN,""})
-        } else if str[index] == "+" {
-            result = append(result,Token{ADD,""})
-        } else if str[index] == "-" {
-            if result[len(result) - 1].Type() == NUM ||
-                result[len(result) - 1].Type() == SYMBOL ||
-                result[len(result) - 1].Type() == TMP ||
-                result[len(result) - 1].Type() == BPAREN {
-                result = append(result,Token{SUB,""})
-            } else {
-                result = append(result,Token{TMP,""})
-            }
-        } else if str[index] == "*" {
-            result = append(result,Token{MULTI,""})
-        } else if str[index] == "/" {
-            result = append(result,Token{DIV,""})
-        } else {
-            fmt.Println("Warn: unknown token in X expression. Token: ", index)
-        }
-    }
-    return result
+	result := make([]Token, 0)
+	for index := 0; index < len(str); index++ {
+		if str[index] == " " || str[index] == "\t" {
+			// Do nothing
+		} else if str[index] == "_" || IsSingleAlpha(str[index]) {
+			begin := index
+			for ; index < len(str) &&
+				(IsSingleAlpha(str[index]) ||
+					IsSingleNum(str[index]) ||
+					str[index] == "_"); index++ {
+			}
+			result = append(result, Token{SYMBOL, strings.Join(str[begin:index], "")})
+			index--
+		} else if IsSingleNum(str[index]) {
+			begin := index
+			for ; index < len(str) && IsSingleNum(str[index]); index++ {
+				// Do nothing
+			}
+			if result[len(result)-1].Type() == TMP {
+				result = append(result, Token{NUM, "-" + strings.Join(str[begin:index], "")})
+			} else {
+				result = append(result, Token{NUM, strings.Join(str[begin:index], "")})
+			}
+			index--
+		} else if str[index] == "(" {
+			result = append(result, Token{FPAREN, ""})
+		} else if str[index] == ")" {
+			result = append(result, Token{BPAREN, ""})
+		} else if str[index] == "+" {
+			result = append(result, Token{ADD, ""})
+		} else if str[index] == "-" {
+			if result[len(result)-1].Type() == NUM ||
+				result[len(result)-1].Type() == SYMBOL ||
+				result[len(result)-1].Type() == TMP ||
+				result[len(result)-1].Type() == BPAREN {
+				result = append(result, Token{SUB, ""})
+			} else {
+				result = append(result, Token{TMP, ""})
+			}
+		} else if str[index] == "*" {
+			result = append(result, Token{MULTI, ""})
+		} else if str[index] == "/" {
+			result = append(result, Token{DIV, ""})
+		} else {
+			fmt.Println("Warn: unknown token in X expression. Token: ", index)
+		}
+	}
+	return result
 }
--- xlexer/xlex_test.go
+++ xlexer/xlex_test.go
@@ -1,38 +1,22 @@
 package xlexer
 
 import (
-    "testing"
-    "fmt"
-    "strings"
+	"strings"
+	"testing"
 )
 
-func TestLex(t *testing.T) {
-    const t1 = 
-        "1+var1/ (7 - -)*8-7-7"
-    var res1 = []Token{
-        Token{NUM,"1"},
-        Token{ADD,""},
-        Token{SYMBOL,"var1"},
-        Token{DIV,""},
-        Token{FPAREN,""},
-        Token{NUM,"7"},
-        Token{SUB,""},
-        Token{TMP,""},
-        Token{BPAREN,""},
-        Token{MULTI,""},
-        Token{NUM,"8"},
-        Token{SUB,""},
-        Token{NUM,"7"},
-        Token{SUB,""},
-        Token{NUM,"7"},
-    }
-    result := Lex(strings.Split(t1,""))
-    for key, value := range result {
-        if res1[key].Type() != value.Type() ||
-           res1[key].Value() != value.Value() {
-            fmt.Println("expecting: ",res1[key])
-            fmt.Println("but got: ",value)
-             fmt.Println("token: ", key)
-        }
-    }
+func TestXLex(t *testing.T) {
+	const t1 = "7--1"
+	var res1 = []Token{
+		Token{NUM, "7"},
+		Token{SUB, ""},
+		Token{NUM, "-1"},
+	}
+	result := Lex(strings.Split(t1, ""))
+	for key, value := range result {
+		if res1[key].Type() != value.Type() ||
+			res1[key].Value() != value.Value() {
+			t.Fatal("expecting: ", res1[key], "but got: ", value, "token: ", key)
+		}
+	}
 }
