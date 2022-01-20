9a219c4fa30eaa296909724475ce134476378d13
zhangzheheng12345@163.com <zhangzheheng12345@163.com>
2022-01-20 21:01:20 +0800

    Update

--- tools/tools.go
+++ tools/tools.go
@@ -1,25 +1,39 @@
 package tools
 
 import (
-    "strings"
+	"strings"
 )
 
 /* Check if a character is a number. */
 func IsSingleNum(str string) bool {
-    return len(str) == 1 && []byte(str)[0] >= 48 && []byte(str)[0] <= 57
+	return len(str) == 1 && []byte(str)[0] >= 48 && []byte(str)[0] <= 57
 }
 
 /* Check if a character is a alpha */
 func IsSingleAlpha(str string) bool {
-    return len(str) == 1 && []byte(str)[0] >= 65 && []byte(str)[0] <= 122
+	return len(str) == 1 && []byte(str)[0] >= 65 && []byte(str)[0] <= 122
 }
 
+/* Return a string of the symbol's name, if there is a symbol. */
 func PickSymbol(str []string, index int) (string, int) {
-    begin := index
+	begin := index
 	for ; index < len(str) &&
 		(IsSingleAlpha(str[index]) ||
-		 IsSingleNum(str[index]) ||
-		 str[index] == "_"); index++ {
+			IsSingleNum(str[index]) ||
+			str[index] == "_"); index++ {
+		// Do nothing
 	}
-    return strings.Join(str[begin:index], ""), index - 1
+	return strings.Join(str[begin:index], ""), index - 1
+}
+
+/* Return a string of the number, if there is a number. */
+func PickNum(str []string, index int) (string, int) {
+	begin := index
+	if str[index] == "-" {
+		index++
+	}
+	for ; index < len(str) && IsSingleNum(str[index]); index++ {
+		// Do nothing
+	}
+	return strings.Join(str[begin:index], ""), index - 1
 }
